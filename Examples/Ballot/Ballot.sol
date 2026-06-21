ea07f667ff28c3c28508ff5f94e8469cfe71fe4b
zoep <zoe.paraskevopoulou@gmail.com>
2026-06-21 13:17:31 +0300

    Examples: add ballot for future use

--- Examples/Ballot/Ballot.sol
+++ Examples/Ballot/Ballot.sol
@@ -1,125 +1,156 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.4;
-contract Purchase {
-    uint public value;
-    address payable public seller;
-    address payable public buyer;
-
-    enum State { Created, Locked, Release, Inactive }
-    // The state variable has a default value of the first member, `State.created`
-    State public state;
-
-    modifier condition(bool condition_) {
-        require(condition_);
-        _;
+pragma solidity >=0.7.0 <0.9.0;
+/// @title Voting with delegation.
+contract Ballot {
+    // This declares a new complex type which will
+    // be used for variables later.
+    // It will represent a single voter.
+    struct Voter {
+        uint weight; // weight is accumulated by delegation
+        bool voted;  // if true, that person already voted
+        address delegate; // person delegated to
+        uint vote;   // index of the voted proposal
     }
 
-    /// Only the buyer can call this function.
-    error OnlyBuyer();
-    /// Only the seller can call this function.
-    error OnlySeller();
-    /// The function cannot be called at the current state.
-    error InvalidState();
-    /// The provided value has to be even.
-    error ValueNotEven();
-
-    modifier onlyBuyer() {
-        if (msg.sender != buyer)
-            revert OnlyBuyer();
-        _;
+    // This is a type for a single proposal.
+    struct Proposal {
+        bytes32 name;   // short name (up to 32 bytes)
+        uint voteCount; // number of accumulated votes
     }
 
-    modifier onlySeller() {
-        if (msg.sender != seller)
-            revert OnlySeller();
-        _;
+    address public chairperson;
+
+    // This declares a state variable that
+    // stores a `Voter` struct for each possible address.
+    mapping(address => Voter) public voters;
+
+    // A dynamically-sized array of `Proposal` structs.
+    Proposal[] public proposals;
+
+    /// Create a new ballot to choose one of `proposalNames`.
+    constructor(bytes32[] memory proposalNames) {
+        chairperson = msg.sender;
+        voters[chairperson].weight = 1;
+
+        // For each of the provided proposal names,
+        // create a new proposal object and add it
+        // to the end of the array.
+        for (uint i = 0; i < proposalNames.length; i++) {
+            // `Proposal({...})` creates a temporary
+            // Proposal object and `proposals.push(...)`
+            // appends it to the end of `proposals`.
+            proposals.push(Proposal({
+                name: proposalNames[i],
+                voteCount: 0
+            }));
+        }
     }
 
-    modifier inState(State state_) {
-        if (state != state_)
-            revert InvalidState();
-        _;
+    // Give `voter` the right to vote on this ballot.
+    // May only be called by `chairperson`.
+    function giveRightToVote(address voter) external {
+        // If the first argument of `require` evaluates
+        // to `false`, execution terminates and all
+        // changes to the state and to Ether balances
+        // are reverted.
+        // This used to consume all gas in old EVM versions, but
+        // not anymore.
+        // It is often a good idea to use `require` to check if
+        // functions are called correctly.
+        // As a second argument, you can also provide an
+        // explanation about what went wrong.
+        require(
+            msg.sender == chairperson,
+            "Only chairperson can give right to vote."
+        );
+        require(
+            !voters[voter].voted,
+            "The voter already voted."
+        );
+        require(voters[voter].weight == 0);
+        voters[voter].weight = 1;
     }
 
-    event Aborted();
-    event PurchaseConfirmed();
-    event ItemReceived();
-    event SellerRefunded();
+    /// Delegate your vote to the voter `to`.
+    function delegate(address to) external {
+        // assigns reference
+        Voter storage sender = voters[msg.sender];
+        require(sender.weight != 0, "You have no right to vote");
+        require(!sender.voted, "You already voted.");
 
-    // Ensure that `msg.value` is an even number.
-    // Division will truncate if it is an odd number.
-    // Check via multiplication that it wasn't an odd number.
-    constructor() payable {
-        seller = payable(msg.sender);
-        value = msg.value / 2;
-        if ((2 * value) != msg.value)
-            revert ValueNotEven();
+        require(to != msg.sender, "Self-delegation is disallowed.");
+
+        // Forward the delegation as long as
+        // `to` also delegated.
+        // In general, such loops are very dangerous,
+        // because if they run too long, they might
+        // need more gas than is available in a block.
+        // In this case, the delegation will not be executed,
+        // but in other situations, such loops might
+        // cause a contract to get "stuck" completely.
+        while (voters[to].delegate != address(0)) {
+            to = voters[to].delegate;
+
+            // We found a loop in the delegation, not allowed.
+            require(to != msg.sender, "Found loop in delegation.");
+        }
+
+        Voter storage delegate_ = voters[to];
+
+        // Voters cannot delegate to accounts that cannot vote.
+        require(delegate_.weight >= 1);
+
+        // Since `sender` is a reference, this
+        // modifies `voters[msg.sender]`.
+        sender.voted = true;
+        sender.delegate = to;
+
+        if (delegate_.voted) {
+            // If the delegate already voted,
+            // directly add to the number of votes
+            proposals[delegate_.vote].voteCount += sender.weight;
+        } else {
+            // If the delegate did not vote yet,
+            // add to her weight.
+            delegate_.weight += sender.weight;
+        }
     }
 
-    /// Abort the purchase and reclaim the ether.
-    /// Can only be called by the seller before
-    /// the contract is locked.
-    function abort()
-        external
-        onlySeller
-        inState(State.Created)
+    /// Give your vote (including votes delegated to you)
+    /// to proposal `proposals[proposal].name`.
+    function vote(uint proposal) external {
+        Voter storage sender = voters[msg.sender];
+        require(sender.weight != 0, "Has no right to vote");
+        require(!sender.voted, "Already voted.");
+        sender.voted = true;
+        sender.vote = proposal;
+
+        // If `proposal` is out of the range of the array,
+        // this will throw automatically and revert all
+        // changes.
+        proposals[proposal].voteCount += sender.weight;
+    }
+
+    /// @dev Computes the winning proposal taking all
+    /// previous votes into account.
+    function winningProposal() public view
+            returns (uint winningProposal_)
     {
-        emit Aborted();
-        state = State.Inactive;
-        // We use call here directly. It is
-        // reentrancy-safe, because it is the
-        // last call in this function and we
-        // already changed the state.
-        (bool success, ) = seller.call{value: address(this).balance}("");
-        require(success);
+        uint winningVoteCount = 0;
+        for (uint p = 0; p < proposals.length; p++) {
+            if (proposals[p].voteCount > winningVoteCount) {
+                winningVoteCount = proposals[p].voteCount;
+                winningProposal_ = p;
+            }
+        }
     }
 
-    /// Confirm the purchase as buyer.
-    /// Transaction has to include `2 * value` ether.
-    /// The ether will be locked until confirmReceived
-    /// is called.
-    function confirmPurchase()
-        external
-        inState(State.Created)
-        condition(msg.value == (2 * value))
-        payable
+    // Calls winningProposal() function to get the index
+    // of the winner contained in the proposals array and then
+    // returns the name of the winner
+    function winnerName() external view
+            returns (bytes32 winnerName_)
     {
-        emit PurchaseConfirmed();
-        buyer = payable(msg.sender);
-        state = State.Locked;
-    }
-
-    /// Confirm that you (the buyer) received the item.
-    /// This will release the locked ether.
-    function confirmReceived()
-        external
-        onlyBuyer
-        inState(State.Locked)
-    {
-        emit ItemReceived();
-        // It is important to change the state first because
-        // otherwise, the contracts called using `call` below
-        // can call in again here.
-        state = State.Release;
-
-        (bool success, ) = buyer.call{value: value}("");
-        require(success);
-    }
-
-    /// This function refunds the seller, i.e.
-    /// pays back the locked funds of the seller.
-    function refundSeller()
-        external
-        onlySeller
-        inState(State.Release)
-    {
-        emit SellerRefunded();
-        // It is important to change the state first because
-        // otherwise, the contracts called using `call` below
-        // can call in again here.
-        state = State.Inactive;
-
-        (bool success, ) = seller.call{value: 3 * value}("");
-        require(success);
+        winnerName_ = proposals[winningProposal()].name;
     }
 }
